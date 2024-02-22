9b24c879c160c4fd4f0fb23c3604814a3c3a098e
huseyinkedik <159181717+huseyinkedik@users.noreply.github.com>
2024-02-22 12:03:14 +0300

    v2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        int a = input.nextInt();
+
+        for (int i = 0; i <= a; i++) {
+            for (int j = 0; j < (a - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for (int i = (a); i >= 0; i--) {
+            for (int j = 0; j <= (a - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i) - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
     }
 }
